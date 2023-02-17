e28d320770f69ec43524fa0d52a1a81590585c39
Ramachandra Maddela <mramachandra9@gmail.com>
2023-02-17 08:25:10 +0530

    first commit

---
+++ src/main/java/com/learning/demo/service/DemoController.java
@@ -0,0 +1,18 @@
+package com.learning.demo.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+	
+
+@GetMapping("/message")
+public String message() {
+	return "demo message!";
+	
+}
+
+}
